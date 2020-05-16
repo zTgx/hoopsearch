1c9c3407a1aa58e506d94d9d0671f68c04305a2d
zTgx <747674262@qq.com>
2020-05-16 18:37:43 +0800

    DELETE: dummy file

--- README.md
+++ README.md
@@ -1,9 +1,2 @@
-# kdb
-A Key-Value Store based on Rocksdb & lmdb.
-
-# dependencies
-* [rust-rocksdb](https://github.com/pingcap/rust-rocksdb)
-* [lmdb-zero](https://github.com/AltSysrq/lmdb-zero)
-* [grpc-rs](https://github.com/pingcap/grpc-rs.git)
-* [protoc-grpcio](https://github.com/mtp401/protoc-grpcio.git)
-* [raft](https://github.com/pingcap/raft-rs)
+# hoopsearch
+A Toy Search Engine
--- examples/n01_rocksdb.rs
+++ examples/n01_rocksdb.rs
@@ -1,26 +1,18 @@
 extern crate rocksdb;
-use rocksdb::{DB, Writable, WriteBatch};
+use rocksdb::{DB, Options};
 
 fn main() {
-    let mut db = DB::open_default("./k.db").unwrap();
-    db.put(b"key1", b"value1");
-
-    match db.get(b"key1") {
-        Ok( Some( v ) ) => println!("retrieved value {}", v.to_utf8().unwrap()),
-        Ok( None )      => println!("value not found."),
-        Err( e )        => println!("operational problem encountered: {}", e),
-    }
-
-    //write batch
+    // NB: db is automatically closed at end of lifetime
+    let path = "./k.db";
     {
-        let mut batch = WriteBatch::new();
-        batch.put(b"A", b"V1");
-        batch.put(b"B", b"V2");
-
-        db.write( &batch ); //atomically commit
+       let db = DB::open_default(path).unwrap();
+       db.put(b"my key", b"my value").unwrap();
+       match db.get(b"my key") {
+           Ok(Some(value)) => println!("retrieved value {}", String::from_utf8(value).unwrap()),
+           Ok(None) => println!("value not found"),
+           Err(e) => println!("operational problem encountered: {}", e),
+       }
+       db.delete(b"my key").unwrap();
     }
-
-    db.delete(b"key1");
-
-
+    // let _ = DB::destroy(&Options::default(), path);
 }
