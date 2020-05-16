b2b672cd5a938f0fbd4ad8845ed20d1d8ee9d569
zTgx <747674262@qq.com>
2020-05-16 21:48:20 +0800

    FIX: search

--- src/main.rs
+++ src/main.rs
@@ -20,14 +20,14 @@ fn main() {
 fn print_lanuch_mascot() {
     let ascii_name = r#"
     hhh       hhh
-    hhh       hhh                                          .d8888b.                                    888
-    hhh       hhh                                         d88P  Y88b                                   888  
-    hhh       hhh                         ppp             Y88b.                                        888
-    hhhhhhhhhhhhh    oooooo       oooooo   ppp    pppppp    "Y888b.    .d88b.   8888b.  888d888 .d8888b 88888b.
+    hhh       hhh                                            .d8888b.                                    888
+    hhh       hhh                                           d88P  Y88b                                   888  
+    hhh       hhh                         ppp               Y88b.                                        888
+    hhhhhhhhhhhhh    oooooo       oooooo   ppp    pppppp     "Y888b.    .d88b.   8888b.  888d888 .d8888b 88888b.
     hhh       hhh  oooooooooo   oooooooooo  ppp pppppppppp      "Y88b. d8P  Y8b     "88b 888P"  d88P"    888 "88b
     hhh       hhh oooooooooooo oooooooooooo ppppppppppppppp       "888 88888888 .d888888 888    888      888  888
     hhh       hhh  oooooooooo   oooooooooo  ppp pppppppppp  Y88b  d88P Y8b.     888  888 888    Y88b.    888  888
-    hhh       hhh   ooooooo      ooooooo    ppp  ppppppp    "Y8888P"   "Y8888  "Y888888 888     "Y8888P 888  888     
+    hhh       hhh   ooooooo      ooooooo    ppp  ppppppp     "Y8888P"   "Y8888  "Y888888 888     "Y8888P 888  888     
                                             ppp   
                                             ppp  
                                             ppp
