43b007778fb1686eb24a15df8217daa491d83faa
zTgx <747674262@qq.com>
2020-05-17 00:25:13 +0800

    FIX: mascot

--- src/main.rs
+++ src/main.rs
@@ -35,17 +35,17 @@ fn print_lanuch_mascot() {
     hhh       hhh
     hhh       hhh                                            .d8888b.                                    888
     hhh       hhh                                           d88P  Y88b                                   888  
-    hhh       hhh                         ppp               Y88b.                                        888
-    hhhhhhhhhhhhh    oooooo       oooooo   ppp    pppppp     "Y888b.    .d88b.   8888b.  888d888 .d8888b 88888b.
+    hhh       hhh                           ppp             Y88b.                                        888
+    hhhhhhhhhhhhh    oooooo       oooooo    ppp  pppppppp    "Y888b.    .d88b.   8888b.  888d888 .d8888b 88888b.
     hhh       hhh  oooooooooo   oooooooooo  ppp pppppppppp      "Y88b. d8P  Y8b     "88b 888P"  d88P"    888 "88b
     hhh       hhh oooooooooooo oooooooooooo ppppppppppppppp       "888 88888888 .d888888 888    888      888  888
     hhh       hhh  oooooooooo   oooooooooo  ppp pppppppppp  Y88b  d88P Y8b.     888  888 888    Y88b.    888  888
-    hhh       hhh   ooooooo      ooooooo    ppp  ppppppp     "Y8888P"   "Y8888  "Y888888 888     "Y8888P 888  888     
+    hhh       hhh   ooooooo      ooooooo    ppp  pppppppp    "Y8888P"   "Y8888  "Y888888 888     "Y8888P 888  888     
                                             ppp   
                                             ppp  
                                             ppp
-                                           ppp
-                                          ppp
+                                            ppp
+                                            ppp
     "#;
     
     info!("{}", ascii_name);    
