d52a8a39c936f929ec6a2df3edbac49ecfb0e16a
zTgx <747674262@qq.com>
2020-06-02 11:52:12 +0800

    添加中文分词 jieba-rs && 更新examples

---
+++ examples/n02_jieba.rs
@@ -0,0 +1,10 @@
+use jieba_rs::Jieba;
+
+fn main() {
+    let jieba = Jieba::new();
+    let words = jieba.cut("我们中出了一个叛徒", false);
+    
+    println!("words: {:?}", words);
+    
+    assert_eq!(words, vec!["我们", "中", "出", "了", "一个", "叛徒"]);
+}
